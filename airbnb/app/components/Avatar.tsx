fdc40a0367958f43734d91ff88da39702f0d557f
Vishnu <vishnupb02001@gmail.com>
2023-08-13 20:34:12 +0530

    navbar-ui completed

---
+++ airbnb/app/components/Avatar.tsx
@@ -0,0 +1,9 @@
+"use cient"
+import Image from "next/image"
+const Avatar = () => {
+    return (
+        <Image className="rounded-full" alt="avatar" width="30" height="30" src="/images/placeholder.jpg" />
+    )
+}
+
+export default Avatar
--- airbnb/app/components/navbar/UserMenu.tsx
+++ airbnb/app/components/navbar/UserMenu.tsx
@@ -1,6 +1,16 @@
 "use client"
+import {useState , useCallback} from "react"
 import {AiOutlineMenu} from "react-icons/ai"
+import Avatar from "../Avatar";
+import MenuItem from "./MenuItem";
 const UserMenu = () =>{
+    const [isOpen , setIsOpen] = useState(false);
+
+    const toggleOpen =useCallback(() => {
+            console.log("toggle workings")
+            setIsOpen(!isOpen);
+        },[]);
+
     return (
         <div className="relative">
             <div className="flex flex-row items-center gap-3">
@@ -9,11 +19,24 @@ const UserMenu = () =>{
                 className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
                     Airbnb your Home
                 </div>
-                <div onClick={()=>{}}
+                <div onClick={()=>{toggleOpen()}}
                 className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer transition hover:shadow-md">
                         <AiOutlineMenu />
+                        <div className="hidden md:block ">
+                            <Avatar />
+                        </div>
                 </div>
             </div>
+            {isOpen && (
+                <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+                    <div className="flex flex-col cursor-pointer">
+                        <>
+                            <MenuItem onClick={()=>{}} label="login"/>
+                            <MenuItem onClick={()=>{}} label="Signup"/>
+                        </>
+                    </div>
+                </div>
+            )} 
         </div>
     )
 }
